f93ef63e2b02d1989558b1cb36860328084b4559
Moedyiu <86594262+gillommnathony@users.noreply.github.com>
2022-01-24 00:50:09 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, request
+import json, time
+import lk21
 
 app = Flask(__name__)
 
-@app.rote('/')
-def index():
-    return "hello world"
+@app.route('/', methods=['GET'])
 
-if __name__ == "__main__":
+def home_page():
+    data_set = {'Page': 'Home', 'Message': 'This is Home Page', 'Timestamp': time.time()}
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+@app.route('/fembed/', methods=['GET'])
+
+def request_page():
+    user_query = str(request.args.get('url'))
+    bypasser = lk21.Bypass()
+    dl_url =bypasser.bypass_fembed(user_query)
+    lst_link = []
+    for i in dl_url:
+        lst_link.append(dl_url[i])
+
+
+
+
+    data_set = dl_url
+    json_dump = json.dumps(data_set)
+    return json_dump
+
+if __name__ == '__main__':
     app.run()
