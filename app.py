d77f2e4550c716a0b11f8a73c6e6427c3b1b45f8
Moedyiu <86594262+gillommnathony@users.noreply.github.com>
2022-01-24 11:25:02 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
 from flask import Flask, request
 import json, time
 import lk21
-from uvicorn.middleware.proxy_headers import ProxyHeadersMiddleware
-app = Flask(__name__)
-if request.headers.getlist("X-Forwarded-For"):
-    ip = request.headers.getlist("X-Forwarded-For")[0]
-else:
-    ip = request.remote_addr
-app.asgi_app = ProxyHeadersMiddleware(app.asgi_app, trusted_hosts=[f"{ip}"])
-@app.route('/', methods=['GET'])
 
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
 
 def home_page():
     if request.headers.getlist("X-Forwarded-For"):
         ip = request.headers.getlist("X-Forwarded-For")[0]
     else:
         ip = request.remote_addr
-    data_set = {'Page': 'Home', 'Message': f'This is Home Page {ip}', 'Timestamp': time.time()}
+    data_set = {'Page': 'Home', 'Message': f'Home Page And IP :  {ip}', 'Timestamp': time.time()}
     json_dump = json.dumps(data_set)
     return json_dump
 
@@ -26,15 +21,7 @@ def request_page():
     user_query = str(request.args.get('url'))
     bypasser = lk21.Bypass()
     dl_url =bypasser.bypass_fembed(user_query)
-    lst_link = []
-    for i in dl_url:
-        lst_link.append(dl_url[i])
-
-
-
-
-    data_set = dl_url
-    json_dump = json.dumps(data_set)
+    json_dump = json.dumps(dl_url)
     return json_dump
 
 if __name__ == '__main__':
