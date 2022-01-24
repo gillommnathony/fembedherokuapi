c439bc528730475cfe7e8531585f8d955573d788
Moedyiu <86594262+gillommnathony@users.noreply.github.com>
2022-01-24 10:41:11 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, request
-from werkzeug.middleware.proxy_fix import ProxyFix
 import json, time
 import lk21
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/', methods=['GET'])
 
